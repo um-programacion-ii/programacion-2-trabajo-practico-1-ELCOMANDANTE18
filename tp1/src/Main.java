8cd8ff029cc8314f1fb019932162c9ca5bc59ed4
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2025-04-09 03:01:57 +0000

    feat: Implementa la clase Auto como subclase de Vehiculo con atributo cantidadPasajeros.

--- tp1/src/Main.java
+++ tp1/src/Main.java
@@ -3,10 +3,13 @@ public class Main {
     public static void main(String[] args) {
         Vehiculo miVehiculo = new Vehiculo("ABC123", "Toyota", 2020, 500.0);
         Camion miCamion = new Camion("DEF456", "Volvo", 2023, 15000.0, true);
+        Auto miAuto = new Auto("GHI789", "Ford", 2022, 0.0, 5);
 
         VehiculoPrinter printer = new VehiculoPrinter();
         printer.imprimir(miVehiculo);
         System.out.println("---");
         printer.imprimir(miCamion);
+        System.out.println("---");
+        printer.imprimir(miAuto);
     }
 }
